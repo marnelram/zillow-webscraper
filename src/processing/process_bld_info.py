a5a0f23696ab52195505ff27412e92c15bd0a7c1
marnelram <marnelram@gmail.com>
2023-05-02 15:57:14 -0700

    changed file names to snake case

--- src/processing/process_bld_info.py
+++ src/processing/process_bld_info.py
@@ -8,112 +8,6 @@ with open("raw-bld-info.json", "r", encoding="utf-8") as f:
 bld_info = json.loads(raw_bld_info)
 
 
-def filter_dict(source_dict: dict, filter_keys: list) -> dict:
-    """filters a dictionary and removes keys not in the filter keys list."""
-    processed_dict = {}
-    for key, value in source_dict.items():
-        if key in filter_keys:
-            processed_dict[key] = value
-    return processed_dict
-
-
-def inherit_subset_dict(
-    superset: dict, subset: dict, subset_key: str, sub_keys: list
-) -> dict:
-    """Inherit the keys from the subset in the key list into the superset.
-
-    Args:
-        superset (dict):
-            the superset dict that contains the subset.
-        subset (dict):
-            the subset dict that is contained by the superset.
-        subset_key (str):
-            the key of the subset dictionary.
-        sub_keys (list):
-            a list of subset keys to keep.
-
-    Returns:
-        dict:
-            A dictionary that includes inherited heys from the subset specified by sub_keys.
-    """
-    processed_dict = superset.copy()
-    processed_subset = filter_dict(subset, sub_keys)
-    processed_dict.update(processed_subset)
-    if subset_key in processed_dict:
-        del processed_dict[subset_key]
-    return processed_dict
-
-
-def split_list_of_subset_dicts(superset: dict, subset_key: str, sub_keys: list) -> list:
-    """Extract a list of dictionaries from a list of subset dictionaries with the specified keys.
-
-    Each element of the list contains keys from both the superset dictionary and the corresponding subset dictionary.  The keys from the subset dictionary are specified by the sub_keys list.
-
-    Args:
-        superset (dict): the superset dictionary that contains the subset.
-
-        an example of the superset dictionary is shown below:
-
-        "superset": {
-            "superset_key_1": "superset_value_1",
-            "subset_key": [
-                {
-                    "subset_dict[0]_key": "subset_dict[0]_value_1".
-                    "subset_dict[0]_sub_keys[0]": "subset_dict[0]_sub_keys[0]_value",
-                    "subset_dict[0]_sub_keys[1]": "subset_dict[0]_sub_keys[1]_value",
-                    ...
-                },
-                {
-                    "subset_dict[1]_key": "subset_dict[0]_alue_1".
-                    "subset_dict[1]_sub_keys[0]": "subset_dict[1]_sub_keys[0]_value",
-                    ...
-                },
-            ]
-            "superset_key_2": "superset_value_2",
-            ...
-        }
-
-        subset_key (str): the key in the superset dictionary that maps to a list of subset dictionaries.
-        sub_keys (list): list of subset keys to keep.
-
-    Returns:
-        list: list of dictionaries that include the inherited keys specified by 'sub_keys'.
-
-        An example of the returned list is shown below:
-
-        [
-            "processed_dicts[0]": {
-                "superset_key_1": "superset_value_1",
-
-                "superset_key_2": "superset_value_2",
-
-                "subset_dict[0]_sub_keys[0]": "subset_dict[0]_sub_keys[0]_value",
-
-                "subset_dict[0]_sub_keys[1]": "subset_dict[0]_sub_keys[1]_value",
-                ...
-            }
-
-            "processed_dicts[1]": {
-                "superset_key_1": "superset_value_1",
-
-                "superset_key_2": "superset_value_2",
-
-                "subset_dict[1]_sub_keys[0]": "subset_dict[1]_sub_keys[0]_value",
-
-                "subset_dict[1]_sub_keys[1]": "subset_dict[1]_sub_keys[1]_value",
-                ...
-            }
-        ]
-    """
-    processed_dicts = []
-    for subset_dict in superset[subset_key]:
-        processed_superset = inherit_subset_dict(
-            superset, subset_dict, subset_key, sub_keys
-        )
-        processed_dicts.append(processed_superset)
-    return processed_dicts
-
-
 def process_address(address: dict) -> dict:
     """returns the address from the address dictionary.
 
@@ -130,11 +24,11 @@ def process_address(address: dict) -> dict:
     )
 
 
-# ***FIX AMENITYSUMMARY WHEN NEW DATA COMES IN
-
-
 def process_amenitySummary(amenitySummary: dict):
     """Returns the amenity summary from the amenitySummary dictionary.  Concatenates the laundry, parking, and pet policies together.
+
+    Args:
+        amenitySummary (dict): the amenity summary dictionary.
 
     The amenitySummary dictionary has the following format:
         "amenitySummary": {
@@ -229,7 +123,9 @@ def process_bld_features(bld: dict):
     processed_bld["walkScore"] = bld["walkScore"]["walkscore"]
     processed_bld["transitScore"] = bld["transitScore"]["transit_score"]
     processed_bld["bikeScore"] = bld["bikeScore"]["bikescore"]
-    processed_bld["amenitySummary"] = process_amenitySummary(bld["amenitySummary"])
+    processed_bld["amenitySummary"] = process_amenitySummary(
+        bld["amenitySummary"]
+    )
     # processed_bld_info_dict = add_bld_att(bld_info_dict)
 
     return processed_bld
@@ -269,7 +165,9 @@ def get_units_from_plans(floor_plan: dict):
     unit_keys = ["unitNumber", "zpid", "availableFrom", "price"]
 
     for unit in floor_plan["units"]:
-        processed_unit = inherit_subset_dict(floor_plan, unit, "units", unit_keys)
+        processed_unit = inherit_subset_dict(
+            floor_plan, unit, "units", unit_keys
+        )
         units.append(processed_unit)
 
     return units
